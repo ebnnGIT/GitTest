fe2d7f8976bf2369676a47965563d6f686d3dc49
Erwin <Erwin@Erwin-PC.fritz.box>
2013-04-04 20:37:44 +0200

    Ausgabe Funktion ergänzt

--- GitTesten/src/Bla.java
+++ GitTesten/src/Bla.java
@@ -4,7 +4,17 @@ public class Bla {
 	
 	public static void main(String[]args){
 		System.out.println("Hab da mal was geändert");
+		zweiteAusgabe();
 	}
+	
+	
+	
+	
+	public static void zweiteAusgabe(){
+		System.out.println("Wird mal Zeit für ne Funktion!");
+		System.out.println("MOIHUI");
+	}
+	
 	
 	
 }
